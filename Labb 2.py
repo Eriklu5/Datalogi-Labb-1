c57291bb6bbb81e930a35bb2fcd6f73af0a91abf
Eriklu5 <eriklu5@kth.se>
2026-08-28 16:10:02 +0200

    ArrayQ-modul

--- Labb 2.py
+++ Labb 2.py
@@ -1,40 +1,10 @@
 #labb 2
 from array import array
-
-class ArrayQ():
-    def __init__(self):
-        self.__items = array("b")
-
-    def is_empty(self):
-        return not self.__items
-
-    def enqueue(self,item):
-            self.__items.append(item)
-
-
-    def dequeue(self):
-        item = self.__items.pop(0)
-        return item
-
-
-
-""""
-q = ArrayQ()
-q.enqueue(1)
-q.enqueue(2)
-print(q.is_empty())
-x = q.dequeue()
-y = q.dequeue()
-if (x == 1 and y == 2):
-    print("OK")
-else:
-    print("FAILED")
-print(q.is_empty())
-"""
+from arrayQFile import ArrayQ
 
 def trolleri():
     kortlek = ArrayQ()
-    kort_ordning_in = input("Vilken ordning ligger korten i?")
+    kort_ordning_in = input("Vilken ordning ligger korten i? ")
     kort_ordning = kort_ordning_in.split(" ")
     for kort in kort_ordning:
         kortlek.enqueue(int(kort))
@@ -44,6 +14,6 @@ def trolleri():
         kortlek.enqueue(kort_i_hand)
         print(kortlek.dequeue(),end=" ")
 
-trolleri()
+trolleri() # in-ordningen 3 1 5 2 4 ger ut-ordningen 1 2 3 4 5
 
-#gjort till och med deluppgift 3
+
--- arrayQFile.py
+++ arrayQFile.py
@@ -1,1 +1,30 @@
+from array import array
 
+class ArrayQ():
+    def __init__(self):
+        self.__items = array("b")
+
+    def is_empty(self):
+        return not self.__items
+
+    def enqueue(self,item):
+            self.__items.append(item)
+
+
+    def dequeue(self):
+        item = self.__items.pop(0)
+        return item
+
+
+if __name__=="__main__":
+    q = ArrayQ()
+    q.enqueue(1)
+    q.enqueue(2)
+    print(q.is_empty())
+    x = q.dequeue()
+    y = q.dequeue()
+    if (x == 1 and y == 2):
+        print("OK")
+    else:
+        print("FAILED")
+    print(q.is_empty())

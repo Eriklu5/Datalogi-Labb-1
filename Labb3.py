b671ff8e386d9922bdebde95d4553c802eab1f43
Eriklu5 <eriklu5@kth.se>
2026-09-04 15:27:54 +0200

    ord ord ord

--- Labb3.py
+++ Labb3.py
@@ -1,25 +1,22 @@
 from bintreeFile import Bintree
 
-def makeTree():
-    tree = Bintree()
-    data = input().strip()
-    while data != "#":
-        tree.put(data)
-        data = input().strip()
-    return tree
 
-def searches(tree):
-    findme = input().strip()
-    while findme != "#":
-        if findme in tree:
-            print(findme, "found")
+svenska = Bintree()
+with open("Datalogi-Labb-1/word3.txt", "r", encoding = "utf-8") as svenskfil:
+    for rad in svenskfil:
+        ordet = rad.strip()                # Ett trebokstavsord per rad
+        if ordet in svenska:
+            print(ordet, end = " ") 
         else:
-            print(findme, "not found")
-        findme = input().strip()
+            svenska.put(ordet)             # in i sökträdet
+print("\n")
 
-def main():
-    tree = makeTree()
-    searches(tree)
-
-main()
-
+engelska = Bintree()
+with open("Datalogi-Labb-1/engelska.txt", "r", encoding = "utf-8") as engelskfil:
+    for rad in engelskfil:
+        orden = rad.strip(" ,!").split(" ")
+        for ord in orden: 
+            if not ord in engelska:
+                engelska.put(ord)
+                if ord in svenska:
+                    print(ord, end = " ")

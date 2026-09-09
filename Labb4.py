90658dfce8bfeed6cbe7d3f4f7686ddb85bf1119
Eriklu5 <eriklu5@kth.se>
2026-09-09 15:55:43 +0200

    Labb 4

--- Labb4.py
+++ Labb4.py
@@ -1,6 +1,7 @@
 from bintreeFile import Bintree
+from linkedQFile import LinkedQ
 
-alfabetet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"]
+alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"]
 svenska = Bintree()
 gamla = Bintree()
 
@@ -12,8 +13,31 @@ with open("Datalogi-Labb-1/word3.txt", "r", encoding = "utf-8") as svenskfil:
         else:
             svenska.put(ordet)             # in i sökträdet
 
-startord = input("Vad är startordet?")
-slutord = input("Vad är slutordet?")
+def makechildren(startord,kö):
+    for index, versal in enumerate(startord):
+        for bokstav in alfabet:
+            nyttord = startord
+            if bokstav == versal:
+                pass
+            else: 
+                nyttord = nyttord[:index] + bokstav + nyttord[(1+index):]
+                if nyttord in svenska and not nyttord in gamla:
+                    kö.enqueue(nyttord)
+                    gamla.put(nyttord)
 
-def makechildern(ord):
-    pass
+
+q = LinkedQ()
+startord = input("Vad är startordet? ")
+slutord = input("Vad är slutordet? ")
+
+
+q.enqueue(startord)
+while not q.isEmpty():
+    word = q.dequeue()
+    makechildren(word, q)
+    if word == slutord:    
+        print("Det finns en väg till", slutord)
+        
+        break
+else:
+    print("Det finns ingen väg till", slutord)

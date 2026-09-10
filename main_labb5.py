a4968bc61465d93064b446cbe6117d0f76bf694c
pgvhgvs <pgvhgvs@kth.se>
2026-09-10 15:02:11 +0200

    Kanske nästan helt färdig labb 5

--- main_labb5.py
+++ main_labb5.py
@@ -28,54 +28,56 @@ with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
             svenska.put(ordet)             # in i sökträdet
 
 
-def makechildren(stamfar,q, slutord):
+def makechildren(stamfar, q, slutord):
     """ In parametrar: noden stamfar och den linkade listan q 
         \n Retur värden: inga
         \n använder ordet lagrat i noden och skapar nya ord genom 
         enbokstavsändring utav nodens ord. Dessa stoppas in i kön om de finns i svenska sökträdet
         """
-    startord = stamfar.ord
-    for index, versal in enumerate(startord):
+    for index, versal in enumerate(stamfar.ord):
         for bokstav in ALFABET:
-            nyttord = startord
+            nyttord = stamfar.ord
             if bokstav == versal:
                 pass
             else: 
                 nyttord = nyttord[:index] + bokstav + nyttord[index + 1:]
-                if nyttord in svenska and nyttord not in gamla:
+                if nyttord == slutord: 
+                    writechain(ParentNode(nyttord, stamfar))
+                    raise SolutionFound            
+                elif nyttord in svenska and not nyttord in gamla:
+                    q.enqueue(ParentNode(nyttord, stamfar))
                     gamla.put(nyttord)
-                    nod = ParentNode(nyttord, stamfar)
-                    q.enqueue(nod)
-                if nyttord == slutord:
-                    writechain(ParentNode(nyttord))
+
+                    
 
 def writechain(slutordsnod):
     # "Ska skrivas rekursivt, så att man får ut kedjan med slutordet sist." Alltså Pre-order liknande???
-    if slutordsnod == None:
-        return
-    writechain(slutordsnod.förälder)
+    if slutordsnod.förälder != None:  
+        writechain(slutordsnod.förälder)
     print(slutordsnod.ord)
 
  
 def main():
 
     q = LinkedQ()
-    startord = input("Skriv startord: ")
-    slutord = input("Skriv slutord: ")
-
+    startord = input("Skriv startord: ").strip().lower()
+    slutord = input("Skriv slutord: ").strip().lower()
+    
     stamfar = ParentNode(startord)
-
     q.enqueue(stamfar)
-    while not q.isEmpty():
-        nod = q.dequeue()
-        if nod.ord == slutord:
-            print("Det finns en väg till", slutord)
-            break
-        makechildren(nod, q, slutord) # Möjligtvis ska makechildren ta in slutord som parameter och sedan ska makechildren göra utskriften "det finns en väg". 
-        # Eftersom i labb 5 beskrivningen står det att makechildren ska, om den hittat slutordet från nya orden, anropa writechain som ger utskrift
+    gamla.put(startord)
 
-    else:
-        print("Det finns ingen väg till", slutord)
+    try:
+        while not q.isEmpty():
+            nod = q.dequeue()
+            if nod.ord == slutord:
+                print("Det finns en väg till", slutord)
+                break
+            makechildren(nod, q, slutord) # Möjligtvis ska makechildren ta in slutord som parameter och sedan ska makechildren göra utskriften "det finns en väg". 
+            # Eftersom i labb 5 beskrivningen står det att makechildren ska, om den hittat slutordet från nya orden, anropa writechain som ger utskrift
 
+        else:
+            print("Det finns ingen väg till", slutord)
+    except SolutionFound: print("Det finns en väg till", slutord)
 
 main()
